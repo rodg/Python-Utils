a8febbbf6bb53da08aacd8c642d574dba115efd6
Rod <41761494+rodg@users.noreply.github.com>
2018-11-04 18:22:39 -0500

    Update deck.py

---
+++ deck.py
@@ -0,0 +1,13 @@
+def make_deck():
+    deck = []
+    suits = 'H','D','C','S'
+    faces = 'J','Q','K','A'
+    values = 2,3,4,5,6,7,8,9,10
+    for suit in suits:
+        for value in values:
+            deck.append((value,suit))
+        for face in faces:
+            deck.append((face,suit))
+    for card in deck:
+        print(card)
+
