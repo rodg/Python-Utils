976564a6dfbd7925d98dd5356bdc892adfe006eb
Rod <41761494+rodg@users.noreply.github.com>
2018-11-04 15:12:48 -0500

    Initial upload of prob.py

---
+++ choose.py
@@ -0,0 +1,15 @@
+def choose( n, k):
+    return fact(n)/(fact(k)*fact(n-k))
+
+#def test(a):
+#    print(_fact(5))
+
+def fact(val):
+    if val < 1:
+        return -1
+    if val is 1:
+        return 1
+    else:
+        return val*fact(val-1)
+
+#test(5)
