cda540f91f113df23cb9686c45577918636f5303
Rod Gangnath <rodg1300@gmail.com>
2018-11-04 20:23:06 -0500

    Reformatted

--- choose.py
+++ choose.py
@@ -1,7 +1,8 @@
-def choose( n, k):
-    return fact(n)/(fact(k)*fact(n-k))
+def choose(n, k):
+    return fact(n) / (fact(k) * fact(n - k))
 
-#def test(a):
+
+# def test(a):
 #    print(_fact(5))
 
 def fact(val):
@@ -10,6 +11,6 @@ def fact(val):
     if val is 1:
         return 1
     else:
-        return val*fact(val-1)
+        return val * fact(val - 1)
 
-#test(5)
+# test(5)
--- deck.py
+++ deck.py
@@ -1,16 +1,12 @@
-
 def make_deck():
     deck = []
-    suits = 'H','D','C','S'
-    faces = 'J','Q','K','A'
-    values = 2,3,4,5,6,7,8,9,10
+    suits = 'H', 'D', 'C', 'S'
+    faces = 'J', 'Q', 'K', 'A'
+    values = 2, 3, 4, 5, 6, 7, 8, 9, 10
     for suit in suits:
         for value in values:
-            deck.append((value,suit))
+            deck.append((value, suit))
         for face in faces:
-            deck.append((face,suit))
+            deck.append((face, suit))
 
     return deck
-
-
-
