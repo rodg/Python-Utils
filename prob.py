8d7c16794e452a2963202a601fbe4ad5f2098f40
Rod Gangnath <rodg1300@gmail.com>
2018-11-05 16:42:37 -0500

    Factorial of 0 should be 1 not an error.

--- prob.py
+++ prob.py
@@ -1,14 +1,13 @@
-#function usage: n choose k = choose(n,k)
-def choose( n, k):
-    return fact(n)/(fact(k)*fact(n-k))
+# function usage: n choose k = choose(n,k)
+def choose(n, k):
+    return fact(n) / (fact(k) * fact(n - k))
 
-#is factorial ya
+
+# is factorial ya
 def fact(val):
-    if val < 1:
+    if val < 0:
         return -1
-    if val is 1:
+    if val is 1 or val is 0:
         return 1
     else:
-        return val*fact(val-1)
-
-
+        return val * fact(val - 1)
